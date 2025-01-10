45c355f21c2d77d21023c51a55a44c72fc0a3695
Ronan <ronanscampos@hotmail.com>
2025-01-10 09:08:52 -0400

    Delete Portifolio directory

--- Portifolio/script.js
+++
@@ -1,6 +0,0 @@
-function ToggleMenu(){
-    const menu = document.querySelector(".mobile_links")
-    const icon = document.querySelector(".mobile_icon")
-    menu.classList.toggle("open")
-    icon.classList.toggle("open")
-}
